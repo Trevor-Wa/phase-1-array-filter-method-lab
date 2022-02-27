b607215588e4cf47cdd63a5fa888fcbb0f6331b8
+ <+>
2022-02-26 20:02:47 -0500

    commit tree 160a7ecb3082a3ad7a664cebfa716fbeebf3a823

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const { objectContaining } = require("expect")
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string){
+    let matchfind = drivers.filter(element => element.toLowerCase() === string.toLowerCase())
+    if (matchfind !== undefined){
+        return matchfind}
+        else {
+            return undefined };
+}
+ 
+function fuzzyMatch(drivers,string){
+    let matchFind = drivers.filter(element => element.startsWith(string))
+            return matchFind};
+
+function matchName(drivers, name){
+    let matchfind = drivers.filter(hometown => hometown.name === name)
+    return matchfind }
